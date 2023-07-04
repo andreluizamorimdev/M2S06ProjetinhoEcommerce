429d5d9a0a16bdb23bcc8b566d33cbb5800efc52
andreluizamorimdev <andreluizamorim.dev@gmail.com>
2023-07-04 18:32:45 -0300

    Adicionado pagina contato ao link dos menus, esquecido anteriormente

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css'
-import PaginaProdutos from './pages/PaginaProdutos/PaginaProdutos.tsx';
 import PaginaInicial from './pages/PaginaInicial/PaginaInicial.tsx';
+import PaginaProdutos from './pages/PaginaProdutos/PaginaProdutos.tsx';
+import PaginaContato from './pages/PaginaContato/PaginaContato.tsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<PaginaInicial />} />
         <Route path='/produtos' element={<PaginaProdutos />} />
+        <Route path='/contato' element={<PaginaContato />} />
       </Routes>
     </Router>
   )
