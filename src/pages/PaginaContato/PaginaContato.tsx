e4d6eed27470d9b142f499e0247adfaa383efadf
andreluizamorimdev <andreluizamorim.dev@gmail.com>
2023-07-04 18:07:19 -0300

    Criação do componente de pagina de contato

---
+++ src/pages/PaginaContato/PaginaContato.tsx
@@ -0,0 +1,16 @@
+import Banner from "../../components/Banner/Banner";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+const PaginaContato = () => {
+    return (
+        <>
+            <Header />
+            <Banner descricao='Entre em Contato com' titulo='nossa equipe' />
+
+            <Footer />
+        </>
+    )
+}
+
+export default PaginaContato;
