0a234a5480e821ef4e0fbebd248c80ded8d321e3
andreluizamorimdev <andreluizamorim.dev@gmail.com>
2023-07-05 22:21:18 -0300

    Modificando o modelo de card produto para bootstrap

--- src/components/ProdutoCard/ProdutoCard.tsx
+++ src/components/ProdutoCard/ProdutoCard.tsx
@@ -3,19 +3,22 @@ import './ProdutoCard.css';
 
 const ProdutoCard = ({id, image, nome, valor, descricao, caracteristicas}: IProduto) => {
     return (
-        <div key={id} className='product-list mt-3'>
-            <div className="row">
-                <div className="col-sm-6 col-md-4 col-lg-3 mb-5">
-                    <div className="product">
-                        <img className="image-product" src={image} alt='imagem do produto'/>
-                        <span className="title-product">{nome}</span>
-                        <span className="price-product">{valor}</span>
-                        <span className="description-product">{descricao}</span>
-                        <span className="characteristics-product">{caracteristicas}</span>
-                        <button className="buy-button">Comprar</button>
+        <div key={id} className="card mb-3 mt-3" style={{minWidth: "540px"}}>
+                <div className="row g-0">
+                    <div className="col-md-4">
+                    <img src={image} className="img-fluid rounded-start" alt="imagem do produto" />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{nome}</h5>
+                            <p className="card-text">R$ {valor}</p>
+                            <p className="card-text">{descricao}</p>
+                            <p className="card-text">{caracteristicas}</p>
+                            <button className="btn btn-primary">Comprar</button>
+                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                        </div>
                     </div>
                 </div>
-            </div>
         </div>
     )
 }
